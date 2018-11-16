567c9882ec7cd5121ebd2da24bb40d285d964280
Tjitte de Vries <tjitte.devries@incentro.com>
2018-11-16 15:09:00 +0100

    fix loading relative includes

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const pug = require('pug');
 const nodeResolve = require('resolve').sync;
 
-function transformPugToHtml(src) {
+function transformPugToHtml(src, filePath, baseDir) {
 	const runtimePath = nodeResolve('pug-runtime');
 	const runtime = "var pug = require(" + JSON.stringify(runtimePath) + ");\n";
 
-	return `${runtime}\n${pug.compile(src, { compileDebug: false, pretty: true, filename: src })}; module.exports = template;`;
+	return `${runtime}\n${pug.compile(src, {
+		compileDebug: false,
+		pretty: true,
+		filename: filePath,
+		basedir: baseDir,
+	})}; module.exports = template;`;
 }
 
 module.exports = {
-	process(src, filePath) {
-		return transformPugToHtml(src, filePath);
+	process(src, filePath, options) {
+    const baseDir = options.roots[0];
+		return transformPugToHtml(src, filePath, baseDir);
 	}
 };
